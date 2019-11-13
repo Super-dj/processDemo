66b3698768b3f6e3770d723c001a47a434b98a8f
Super-dj <1715461621@qq.com>
2019-11-13 22:51:18 +0800

    签到页面

---
+++ qiandao/js/index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var btn = document.getElementById('btn-right');
+    btn.onclick = function () {
+        var ball = document.getElementsByClassName('ball')[0];
+        console.log(ball)
+    }
+}
